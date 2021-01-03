9c801742d088d6699beb02aff3bf7697850562df
OG-coder-TE <72239378+OG-coder-TE@users.noreply.github.com>
2021-01-03 17:51:41 -0500

    Create Transfer.java

---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/models/Transfer.java
@@ -0,0 +1,56 @@
+package com.techelevator.tenmo.models;
+
+import javax.validation.Valid; 
+
+import javax.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public class Transfer {
+	
+	private Integer transferId;
+	private Integer transferType;
+	private Integer transferStatus;
+	private Integer accountFrom;
+	private Integer accountTo;
+	private BigDecimal amount;
+	
+	
+	public Integer getTransferId() {
+		return transferId;
+	}
+	public void setTransferId(Integer transferId) {
+		this.transferId = transferId;
+	}
+	public Integer getTransferType() {
+		return transferType;
+	}
+	public void setTransferType(Integer transferType) {
+		this.transferType = transferType;
+	}
+	public Integer getTransferStatus() {
+		return transferStatus;
+	}
+	public void setTransferStatus(Integer transferStatus) {
+		this.transferStatus = transferStatus;
+	}
+	public Integer getAccountFrom() {
+		return accountFrom;
+	}
+	public void setAccountFrom(Integer accountFrom) {
+		this.accountFrom = accountFrom;
+	}
+	public Integer getAccountTo() {
+		return accountTo;
+	}
+	public void setAccountTo(Integer accountTo) {
+		this.accountTo = accountTo;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	
+}
